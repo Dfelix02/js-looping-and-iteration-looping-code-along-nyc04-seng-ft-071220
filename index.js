d80b4a6a37549c4485c1e9bd8cd43896d910e8b7
David Felix <david.felix02@hotmail.com>
2020-09-13 19:30:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function writeCards(names,event){
   for(let i=0;i<names.length;i++){
-    console.log (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    console.log (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
     //names[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`; 
   }
   //return names;
