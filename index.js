297731322c339b9679f7ceb362efbc29d852fdcc
David Felix <david.felix02@hotmail.com>
2020-09-13 19:14:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function writeCards(names){
-   i = 0
+  let i = 0
   while(i<=names.length){
     return console.log(names[i])
     names--
@@ -9,7 +9,7 @@ function writeCards(names){
 
 
 function countDown(num){
-  i = 0
+  let i = 0
   while(i<=num.length){
     return console.log(num[i])
     num--
